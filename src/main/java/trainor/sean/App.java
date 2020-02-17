7073c6ae33a8e162098de13bc5d5eb868fc229df
Dogfoger <seanr.trainor4@gmail.com>
2020-02-17 14:04:27 +0200

    First Commit

---
+++ src/main/java/trainor/sean/App.java
@@ -0,0 +1,14 @@
+package trainor.sean;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
